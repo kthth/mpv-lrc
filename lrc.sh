23a61e7371372105ea953687023dbbba48613001
Klaus Thoden <klaus@sputnik>
2025-02-12 14:34:50 +0100

    Fix indents

--- lrc.sh
+++ lrc.sh
@@ -4,18 +4,18 @@ mediafile=$(printf %s\\n '{ "command": ["get_property", "path"] }' | socat - UNI
 mediadir=$(printf %s\\n '{ "command": ["get_property", "working-directory"] }' | socat - UNIX-CONNECT:/tmp/mpv-socket | jq -r .data) || exit 1
 
 case $mediafile in
-	/*) mediapath=$(printf %s "$mediafile" );;
-	*) mediapath=$(printf %s/%s "$mediadir" "$mediafile" );;
+  /*) mediapath=$(printf %s "$mediafile" );;
+  *) mediapath=$(printf %s/%s "$mediadir" "$mediafile" );;
 esac
 case $lrc_path in
-    */'(unavailable)') exit 1 ;;
+  */'(unavailable)') exit 1 ;;
 esac
 lrc_path=${mediapath%.*}.lrc
 if [ -e "$lrc_path" ]; then
-    exec $EDITOR "$lrc_path"
+  exec $EDITOR "$lrc_path"
 else
-    python3 ~/src/dev/kimono/lrcmaker.py "$mediapath"
-    exec $EDITOR + "$lrc_path"
+  python3 ~/src/dev/kimono/lrcmaker.py "$mediapath"
+  exec $EDITOR + "$lrc_path"
 fi
 
 #metadata=$(printf %s\\n '{ "command": ["get_property", "metadata"] }' \
